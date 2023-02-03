cd2a9df27738c9313aec177096d6bf9898599933
Brad Gessler <bradgessler@gmail.com>
2023-02-03 11:16:10 -0800

    Commit page that displays all static assets and static asset configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def static_asset_url(path)
+    URI(request.url).tap do |u|
+      u.path = File.join("/", Pathname.new(path).relative_path_from(Rails.root.join("public")))
+    end.to_s
+  end
 end
